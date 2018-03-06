96226953ad4444a3028f1a6ee9fc218d8bbbe180
Angel Antezana Flores <ghehahanghel@gmail.com>
2018-03-06 02:05:34 -0400

    2.-GenerarComponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HeaderComponent } from './header/header.component';
+import { UserFormEditComponent } from './user-form-edit/user-form-edit.component';
+import { UserFormCreateComponent } from './user-form-create/user-form-create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    HeaderComponent,
+    UserFormEditComponent,
+    UserFormCreateComponent
   ],
   imports: [
     BrowserModule,
