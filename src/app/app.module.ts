3c9b31f1774a3a2f101a2c39911b2f0bde65c577
Angel Antezana Flores <ghehahanghel@gmail.com>
2018-03-06 19:40:14 -0400

    2.-Generar un commponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormEditComponent } from './user-form-edit/user-form-edit.component';
+import { UserFormCreateComponent } from './user-form-create/user-form-create.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserListComponent,
+    UserFormEditComponent,
+    UserFormCreateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-form-create/user-form-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sky-user-form-create',
+  templateUrl: './user-form-create.component.html',
+  styleUrls: ['./user-form-create.component.css']
+})
+export class UserFormCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
