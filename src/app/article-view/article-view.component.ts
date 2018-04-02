73523ba4e15cbc0767231b32f1cbaf1a54e2c661
Marco Antonio Aguilar Zambrana <marco.aguilarz@gmail.com>
2018-04-01 19:38:51 -0800

    modificando el ArticleView, reordenando los campos

--- src/app/article-view/article-view.component.ts
+++ src/app/article-view/article-view.component.ts
@@ -1,41 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { ArticleService } from '../services/article.service';
-import { ActivatedRoute } from '@angular/router';
-import { Article } from '../model/article';
+import { Component, OnInit } from "@angular/core";
+import { ArticleService } from "../services/article.service";
+import { ActivatedRoute } from "@angular/router";
+import { Article } from "../model/article";
+import { UserService } from "../services/user.service";
+import { User } from "../model/user";
 
 @Component({
-  selector: 'sky-article-view',
-  templateUrl: './article-view.component.html',
-  styleUrls: ['./article-view.component.css']
+  selector: "sky-article-view",
+  templateUrl: "./article-view.component.html",
+  styleUrls: ["./article-view.component.css"]
 })
 export class ArticleViewComponent implements OnInit {
+  //author
+  userid: string;
+  userViewed: User = {
+    name: "",
+    lastname: "",
+    username: "",
+    email: "",
+    avatar: ""
+  };
+
   articleid: string;
   articleViewed: Article = {
-    title: '',
-    content:''
+    title: "",
+    content: "",
+    author: this.userViewed
   };
 
   constructor(
     private activateroute: ActivatedRoute,
-    private articleservice: ArticleService
-  ) { }
+    private articleservice: ArticleService,
+    private userservice: UserService
+  ) {}
 
   ngOnInit() {
-    this.activateroute.params
-      .subscribe(
-        (paramss) => {
-          this.articleid = paramss.id;
-          this.articleservice.getArticleById(this.articleid)
-            .subscribe(
-              (response) => {
-                console.log('respuesta del servidor: ', response);
-                this.articleViewed = response.data;
-              }, (error) => {
-                console.log('Error del servidor', error);
-              }
-            );
+    this.activateroute.params.subscribe(paramss => {
+      this.articleid = paramss.id;
+      this.articleservice.getArticleById(this.articleid).subscribe(
+        response => {
+          console.log("respuesta del servidor: ", response);
+          this.articleViewed = response.data;
+
+          //author
+          this.userid = '5ab7e9937067c40c803968c9';//this.articleViewed.author._id;
+          this.userservice.getUserById(this.userid).subscribe(
+            response1 => {
+              console.log("AUTHOR - respuesta del servidor: ", response1);
+              this.userViewed = response1.data;
+            },
+            error => {
+              console.log("AUTHOR - Error del servidor", error);
+            }
+          );
+          // fin author
+        },
+        error => {
+          console.log("Error del servidor", error);
         }
       );
-  }
+    });
 
+    //author
+    this.activateroute.params.subscribe(paramss => {
+      this.userid = '5ab7e9937067c40c803968c9';//this.articleViewed.author._id;
+      this.userservice.getUserById(this.userid).subscribe(
+        response => {
+          console.log("AUTHOR - respuesta del servidor: ", response);
+          this.userViewed = response.data;
+        },
+        error => {
+          console.log("AUTHOR - Error del servidor", error);
+        }
+      );
+    });
+  }
 }
